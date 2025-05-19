0f8f7bf5b54fc5c5551bd0bf0314b323c0b159cb
Junior Morais <demoraisjunior@outlook.com>
2025-05-19 16:36:00 -0300

    refactor(Person): defensive developer for domain class

--- src/main/java/br/com/payments/core/domain/entities/Person.java
+++ src/main/java/br/com/payments/core/domain/entities/Person.java
@@ -7,12 +7,23 @@ import java.util.UUID;
 
 import static java.util.Objects.requireNonNull;
 
-public record Person(Long id, UUID uuid, String name, String document,
-                     PersonTypeEnum type, String email, BigDecimal balance) {
+public final class Person {
+    private Long id;
+    private UUID uuid;
+    private String name;
+    private String document;
+    private PersonTypeEnum type;
+    private String email;
+    private BigDecimal balance;
 
-    public Person {
-        requireNonNull(document, "Document is required");
-        requireNonNull(email, "Email is required");
+    private Person(Long id, UUID uuid, String name, String document,
+                   PersonTypeEnum type, String email, BigDecimal balance) {
+        this.id = id;
+        this.uuid = uuid;
+        this.name = requireNonNull(name, "Name is required");
+        this.document = requireNonNull(document, "Document is required");
+        this.type = requireNonNull(type, "Type is required");
+        this.email = requireNonNull(email, "Email is required");
     }
 
     public static Person of(Long id, UUID uuid, String name, String document,
@@ -25,18 +36,46 @@ public record Person(Long id, UUID uuid, String name, String document,
         return new Person(null, UUID.randomUUID(), name, document, type, email, BigDecimal.ZERO);
     }
 
-    private boolean hasSufficientFunds(BigDecimal value) {
-        return balance.compareTo(value) >= 0;
-    }
-
     public Person credit(BigDecimal value) {
-        return new Person(id, uuid, name, document, type, email, balance.add(value));
+        return Person.of(id, uuid, name, document, type, email, balance.add(value));
     }
 
     public Person debit(BigDecimal value) {
         if (!hasSufficientFunds(value)) {
             throw new IllegalArgumentException("Payer has no sufficient balance");
         }
-        return new Person(id, uuid, name, document, type, email, balance.subtract(value));
+        return Person.of(id, uuid, name, document, type, email, balance.subtract(value));
+    }
+
+    private boolean hasSufficientFunds(BigDecimal value) {
+        return balance.compareTo(value) >= 0;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public UUID uuid() {
+        return uuid;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String document() {
+        return document;
+    }
+
+    public PersonTypeEnum type() {
+        return type;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public BigDecimal balance() {
+        return balance;
     }
 }
